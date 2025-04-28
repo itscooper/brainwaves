8e5ff8a8bd265628097e895b02496903cc716ef0
itscooper <chris.cooper@owasp.org>
2025-04-28 23:51:49 +0100

    Client mount point fix

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -98,8 +98,8 @@ app = FastAPI(
     version="0.0.1"
 )
 
-# Mount static files
-app.mount("/c", StaticFiles(directory="/client", html=True), name="static")
+# Mount static files using absolute path matching the Dockerfile
+app.mount("/c", StaticFiles(directory="/app/client", html=True), name="static")
 
 # Redirect root to index.html
 @app.get("/")
